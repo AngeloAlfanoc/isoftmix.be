3ade42506655ef6f7286dd3bc19b0dbeaea965ec
Alfano Angelo <alfanoangeloc@gmail.com>
2020-05-18 13:21:40 +0200

    add static website

---
+++ precache-manifest.3ed8946bf6a3bfaf35b54d177f2f1887.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73915d5be99c5f9cd38fd87a880c3df9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "414a2c4d0a0f2c78e093",
+    "url": "/static/css/2.cfc6d871.chunk.css"
+  },
+  {
+    "revision": "312b0d15f0f0353be2d0",
+    "url": "/static/css/main.d24a71f3.chunk.css"
+  },
+  {
+    "revision": "414a2c4d0a0f2c78e093",
+    "url": "/static/js/2.c37c7a87.chunk.js"
+  },
+  {
+    "revision": "a4d90fece219746f59c3dfd7aa769bba",
+    "url": "/static/js/2.c37c7a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312b0d15f0f0353be2d0",
+    "url": "/static/js/main.305182b4.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "d9af9838bcdc0ad4b9b1902eaba9666b",
+    "url": "/static/media/arc.d9af9838.svg"
+  },
+  {
+    "revision": "ea2dade83304eaddc336cf34859ed367",
+    "url": "/static/media/bgwater.ea2dade8.mp4"
+  },
+  {
+    "revision": "3b1efb09e82cd4ffc5254eb8ecbfc31b",
+    "url": "/static/media/logo.3b1efb09.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6350e9e0f15f85999dbbd700200ada0a",
+    "url": "/static/media/thumb.6350e9e0.jpg"
+  }
+]);
